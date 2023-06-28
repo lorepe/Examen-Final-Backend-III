4ef771def3be50a057f1803fad46ab3cf4db6411
lorepe <loreperez.2003@hotmail.com>
2023-06-27 20:33:48 -0500

    repository odontologo

--- internal/odontologo/service.go
+++ internal/odontologo/service.go
@@ -1,1 +1,24 @@
 package odontologo
+
+import (
+	"Final/internal/domain"
+	"Final/internal/odontologo"
+)
+
+type SeviceOdontologo interface {
+	//FIXME revisar error
+	GetAll() []domain.Odontologo
+}
+
+type service struct{
+	repo RepositoryOdontologo
+}
+
+func NewService(repository RepositoryOdontologo)SeviceOdontologo{
+	return &service{repository}
+}
+
+func (s *service) GetAll() ([]domain.Odontologo){
+	odontologos:=s.repo.GetAll()
+	return odontologos
+}
