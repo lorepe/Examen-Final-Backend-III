d60c9df55c612ce2fcdd016975758fbd575d1086
mceciabate <m.cecilia.abate@gmail.com>
2023-06-27 22:59:53 -0300

    group odontologo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	server "Final/cmd/server/odontologoHandler"
+	"Final/internal/odontologo"
+	"Final/pkg/store"
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
+	//repo/service para odontologo
+	//FIXME pasar a env
+	db, err := sql.Open("mysql", "root:root@tcp(serverPort:3306)/clinica")
+	if err != nil {
+		panic(err)
+	}
+	storage := store.NewSqlStore(db)
+
+	repoOdontologo := odontologo.NewRepository(storage)
+	serviceOdontologo := odontologo.NewService(repoOdontologo)
+	odontologoHandler := server.NewOdontologoHandler(serviceOdontologo)
+
+	//FIXME pasar a new
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
+	r.GET("", func(c *gin.Context) { c.String(200, "Bienvenido a la Clínica Odontológica") })
+
+	odontologos := r.Group("/odontologos")
+	{
+		odontologos.GET("", odontologoHandler.GetAll())
+	}
+
+	//TODO hacer una variable del puerto
+	r.Run(":8080")
 
 }
-
- 
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module Final
 
 go 1.20
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/go-sql-driver/mysql v1.7.1
+)
 
 require (
 	github.com/bytedance/sonic v1.9.2 // indirect
