df86c1b4cc7a0cfc0feb3f4e0350254b5f7c24e6
lorepe <loreperez.2003@hotmail.com>
2023-06-28 13:20:59 -0500

    getAll pacientes

---
+++ internal/paciente/repository.go
@@ -0,0 +1,28 @@
+package paciente
+
+import (
+	"Final/internal/domain"
+	"Final/pkg/store"
+	"errors"
+
+	"golang.org/x/mod/sumdb/storage"
+)
+
+type RepositoryPaciente interface {
+	GetAll() ([]domain.Paciente,error)
+}
+type repository struct{
+	storage store.StoreInterface
+}
+
+func NewRepository(storage store.StoreInterface) RepositoryPaciente {
+	return &repository{storage}	
+}
+
+func (r *repository) GetAll()([]domain.Paciente,error) {
+	pacientes, err:= r.storage.GetAllPacientes()
+	if err != nil {
+		return []domain.Paciente{},errors.New("list not found")
+	}	
+	return pacientes, nil
+}
