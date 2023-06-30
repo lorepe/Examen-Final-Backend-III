b33311be888c6f5911bd146491ea191847d48ae6
mceciabate <m.cecilia.abate@gmail.com>
2023-06-30 19:52:31 -0300

    error

--- internal/domain/turno.go
+++ internal/domain/turno.go
@@ -2,10 +2,10 @@ package domain
 
 type Turno struct {
 	Id int `json:"id"`
-	// Paciente    Paciente   `json:"paciente" binding:"required"`
-	PacienteID int `json:"paciente_id" binding:"required"`
-	// Odontologo  Odontologo `json:"odontologo" binding:"required"`
-	OdontologoID int    `json:"odontologo_id" binding:"required"`
+	Paciente    Paciente   `json:"paciente" binding:"required"`
+	// PacienteID int `json:"paciente_id" binding:"required"`
+	Odontologo  Odontologo `json:"odontologo" binding:"required"`
+	// OdontologoID int    `json:"odontologo_id" binding:"required"`
 	FechaHora    string `json:"fecha_hora" binding:"required"`
 	Descripcion  string `json:"descripcion" binding:"required"`
 }
