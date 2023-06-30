545d3937ddf9471c90d8ff6dbbca9a6d964b7b46
mceciabate <m.cecilia.abate@gmail.com>
2023-06-30 17:33:19 -0300

    estructura turnos

--- internal/domain/turno.go
+++ internal/domain/turno.go
@@ -1,9 +1,11 @@
 package domain
 
 type Turno struct {
-	Id          int        `json:"id"`
-	Paciente    Paciente   `json:"paciente" binding:"required"`
-	Odontologo  Odontologo `json:"odontologo" binding:"required"`
-	FechaHora   string     `json:"fecha_hora" binding:"required"`
-	Descripcion string     `json:"descripcion" binding:"required"`
+	Id int `json:"id"`
+	// Paciente    Paciente   `json:"paciente" binding:"required"`
+	PacienteID int `json:"paciente_id" binding:"required"`
+	// Odontologo  Odontologo `json:"odontologo" binding:"required"`
+	OdontologoID int    `json:"odontologo_id" binding:"required"`
+	FechaHora    string `json:"fecha_hora" binding:"required"`
+	Descripcion  string `json:"descripcion" binding:"required"`
 }
